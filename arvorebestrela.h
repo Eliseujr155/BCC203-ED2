cb4e9048e0054fd6098f5fc30931555832fb3226
Eliseujr155 <eliseu2807@gmail.com>
2026-04-27 18:58:58 -0300

    Update arvorebestrela.h

--- arvorebestrela.h
+++ arvorebestrela.h
@@ -2,21 +2,22 @@
 #define ARVOREBESTRELA_H
 #include "registro.h"
 
-#define MM 100 /* Ordem para paginas internas */
+#define MM 100 
 
-/* Tipos de pagina conforme slide 54 [cite: 730] */
+// Define se a página é um nó de índice (só chaves) ou folha (dados completos)
 typedef enum {Interna, Externa} TipoIntExt;
 
 typedef struct TipoPaginaEstrela* TipoApontadorEstrela;
+
 typedef struct TipoPaginaEstrela {
-    TipoIntExt Pt; /* Indica se e interna ou externa [cite: 737] */
+    TipoIntExt Pt; // Variável para controle do tipo do nó
     union {
-        struct { /* Pagina de indice: apenas chaves [cite: 741, 742] */
+        struct { // Estrutura para os nós de cima (índice)
             int ni;
-            int ri[MM];
+            int ri[MM]; // Guarda apenas os inteiros das chaves para economizar RAM
             TipoApontadorEstrela pi[MM + 1];
         } U0;
-        struct { /* Pagina de dados: registros completos [cite: 745, 746] */
+        struct { // Estrutura para os nós folha (onde o registro de 6kb realmente está)
             int ne;
             Registro re[MM];
         } U1;
