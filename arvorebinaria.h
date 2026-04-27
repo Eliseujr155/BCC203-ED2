ec8df79b8b456249e5dc2d7a8f4d4b03fe462eca
Eliseujr155 <eliseu2807@gmail.com>
2026-04-27 18:48:57 -0300

    Create arvorebinaria.h

---
+++ arvorebinaria.h
@@ -0,0 +1,15 @@
+#ifndef ARVOREBINARIA_H
+#define ARVOREBINARIA_H
+#include "registro.h"
+
+/* No da arvore adaptado para armazenar enderecos de disco  */
+typedef struct {
+    Registro registro;
+    long esquerda;  /* Posicao no arquivo (-1 para NULL) */
+    long direita;   /* Posicao no arquivo (-1 para NULL) */
+} NoArquivo;
+
+void inserirBinaria(const char *nomeArq, Registro reg);
+Registro* buscarBinaria(const char *nomeArq, int chave);
+
+#endif
